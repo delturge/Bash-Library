8e4c2399de2b4020d3c1dc12c4f1ec62c275dd93
delturge <anthony_rutledge_101@yahoo.com>
2020-03-10 13:38:11 -0400

    Update Entities.sh

--- Entities/Entities.sh
+++ Entities/Entities.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Anthony Rutledge
+# aerutledge101@gmail.com
+#
+# https://www.linkedin.com/in/anthony-rutledge-2988b0125/
+# https://stackoverflow.com/users/2495645/anthony-rutledge
+#
+# The base library of the bash library.
+#
+# 1) Loads the Linux entity libraries.
+# 
+# ##########################################################
+############################################################
+
 # . Service.sh
 # . User.sh
 . Process.sh
