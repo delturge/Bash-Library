a2bd19db07d4243eebe3753703fe0e2c7c30892b
delturge <anthony_rutledge_101@yahoo.com>
2020-03-10 13:19:39 -0400

    Create Datatype.sh

---
+++ Datatypes/Datatype.sh
@@ -0,0 +1,9 @@
+# @version 1.0
+# @author Anthony E. Rutledge
+# @Copyright Anthony E. Rutledge 3/7/2000 @ 1800 EST All Rights Reserved
+
+. String.sh
+. List.sh
+. Array.sh
+. File.sh
+. TextFile.sh
