c7d7a92b8e9c5fcd517db46b7e03155b188dd745
delturge <anthony_rutledge_101@yahoo.com>
2020-03-10 21:47:37 -0400

    Fix error with library file.

--- Entities/User.sh
+++ Entities/User.sh
@@ -25,43 +25,92 @@ function getUser ()
 function isUser ()
 {
     declare USER=$1
-    getUser $user &> /dev/null
+    getUser $USER &> /dev/null
+}
+
+function setPassMaxDays ()
+{
+    declare -r DAYS=$1
+
+    declare -r TARGET_LINE="^PASS_MAX_DAYS\t[0-9]+?"
+    declare -r SUBSTITUTION="PASS_MAX_DAYS\t${DAYS}"
+    declare -r INPUT_FILE=/etc/login.defs
+
+    if isConfigurable $INPUT_FILE
+    then 
+        updateRecord $(getLineNumber $TARGET_LINE $INPUT_FILE) $TARGET_LINE $SUBSTITUTION $INPUT_FILE
+        return $?
+    fi
+    
+    errorMessage "Error: Unable to set PASS_MAX_DAYS to ${DAYS} in ${INPUT_FILE}. Check permissions."
+    return 2
+}
+
+function setPassWarnAge ()
+{
+    declare -r DAYS=$1
+
+    declare -r TARGET_LINE="^PASS_WARN_AGE\t[0-9]+?"
+    declare -r SUBSTITUTION="PASS_WARN_AGE\t${DAYS}"
+    declare -r INPUT_FILE=/etc/login.defs
+
+    if isConfigurable $INPUT_FILE
+    then 
+        message "Setting PASS_WARN_AGE to ${DAYS}."
+        updateRecord $(getLineNumber $TARGET_LINE $INPUT_FILE) $TARGET_LINE $SUBSTITUTION $INPUT_FILE
+        return $?
+    fi
+    
+    errorMessage "Error: Unable to set PASS_WARN_AGE to ${DAYS} in ${INPUT_FILE}. Check permissions."
+    return 2
 }
 
 function setPasswordDefaults ()
 {
-    grep 'PASS_' login.defs
-    message "Setting PASS_MAX_DAYS to 90."
-    sed -i "s/^PASS_MAX_DAYS\t99999/PASS_MAX_DAYS\t90/" login.defs
-    message "Setting PASS_WARN_AGE to 10."
-    sed -i "s/^PASS_WARN_AGE\t7/PASS_MAX_DAYS\t10/" login.defs
-    grep 'PASS_' login.defs
-    newline
-    sleep 5
+    declare -r PASS_MAX_DAYS=$1
+    declare -r PASS_WARN_AGE=$2
+
+    message "Setting PASS_MAX_DAYS to ${PASS_MAX_DAYS}."
+
+    if setPassMaxDays $PASS_MAX_DAYS # 90 
+    then
+        message "PASS_MAX_DAYS set to ${PASS_MAX_DAYS} days."
+    else
+        errorMessage "Error: Unable to set PASS_MAX_DAYS to ${PASS_MAX_DAYS} in. Check permissions in /etc/login.defs"
+    fi
+
+    if setPassWarnAge $PASS_WARN_AGE # 10
+    then
+        message "PASS_WARN_AGE set to ${PASS_WARN_AGE} days."
+    else
+        errorMessage "Error: Unable to set PASS_WARN_AGE to ${PASS_WARN_AGE} in. Check permissions in /etc/login.defs"
+    fi
 }
 
 function showChangePasswordReport ()
 {
-    if [[ $1 == 0 ]]
+    if (( $1 == 0 ))
     then 
         message "Password changed successfully!"
     else
-        message "Failed to change password!"
+        errorMessage "Failed to change password!"
         exit
     fi
 }
 
 function setPassword ()
 {
-    passwd -S $1
+    declare -r USER=$1
+
+    passwd -S $USER
     sleep 2
 
-    message "Setting password for $1 ..."
+    message "Setting password for $USER ..."
 
-    passwd $1
+    passwd $USER
     showChangePasswordReport $?
 
-    passwd -S $1
+    passwd -S $USER
     sleep 2
 
 }
@@ -79,8 +128,10 @@ function setManyPasswords ()
 
 function configureHomeDir()
 {
-    chown $1:$1 /home/${1}
-    chmod 700 /home/${1}
+    declare -r USER=$1
+
+    chown ${USER}:${USER} /home/${USER}
+    chmod 700 /home/${USER}
 }
 
 function secureHomeDirs ()
@@ -105,154 +156,24 @@ function secureHomeDirsFromFile ()
 
 function showUseraddReport ()
 {
-    if [[ $1 == 0 ]] then 
-        message "User added successfully."
-    else
-        message "Error: Failed to create the user and group!"
-        exit
-    fi
-}
-
-function addRegularUser ()
-{
-    if [[ ! `grep "^${1}:x:" /etc/passwd` ]] then
-        message "Adding $1 to /etc/passwd"
-        useradd -d /home/$1 -e 2020-01-01 -c "Normal user." -s /bin/sh -U $1
-        showUseraddReport $?
-        showUser $1
-        sleep 2
-        return 0
-    fi
-
-    message "The $1 user already exist! No user added."
-}
-
-function addAccounts ()
-{
-    message "Adding users: ${*}"
-
-    for user in "$@"
-    do
-        addRegularUser `echo $user | tr -d [:space:]`
-    done
-}
-
-function addAccountsFromFile ()
-{
-    message "Adding users from this file: ${1}"
-
-    usersString=$(fileToString $1)
-    read -a users <<< $usersString
-    addAccounts "${users[@]}"
-}
-
-function addGroup ()
-{
-    groupadd -g $2 $1
-}
-
-function addToGroup ()
-{
-    group=$1
-    shift
-
-    for $user in "$@"
-    do
-        usermod -G $group $user
-    done
-}
-function setPasswordDefaults ()
-{
-    grep 'PASS_' login.defs
-    message "Setting PASS_MAX_DAYS to 90."
-    sed -i "s/^PASS_MAX_DAYS\t99999/PASS_MAX_DAYS\t90/" login.defs
-    message "Setting PASS_WARN_AGE to 10."
-    sed -i "s/^PASS_WARN_AGE\t7/PASS_MAX_DAYS\t10/" login.defs
-    grep 'PASS_' login.defs
-    newline
-    sleep 5
-}
-
-function showChangePasswordReport ()
-{
-    if [[ $1 == 0 ]]
+    if (( $1 == 0 ))
     then 
-        message "Password changed successfully!"
-    else
-        message "Failed to change password!"
-        exit
-    fi
-}
-
-function setPassword ()
-{
-    passwd -S $1
-    sleep 2
-
-    message "Setting password for $1 ..."
-
-    passwd $1
-    showChangePasswordReport $?
-
-    passwd -S $1
-    sleep 2
-
-}
-
-function setManyPasswords ()
-{
-    message "Setting passwords for $*"
-
-    for user in "$@"
-    do
-        setPassword $user
-        newline
-    done
-}
-
-function configureHomeDir()
-{
-    chown $1:$1 /home/${1}
-    chmod 700 /home/${1}
-}
-
-function secureHomeDirs ()
-{
-    message "Securing home directories for $*"
-
-    for user in "$@"
-    do
-        configureHomeDir $user
-        newline
-    done
-}
-
-function secureHomeDirsFromFile ()
-{
-    message "Securing home directories from this file: ${1}"
-
-    usersString=$(fileToString $1)
-    read -a users <<< $usersString
-    secureHomeDirs "${users[@]}"
-}
-
-function showUseraddReport ()
-{
-    if [[ $1 == 0 ]] then 
         message "User added successfully."
     else
-        message "Error: Failed to create the user and group!"
-        exit
+        errorMessage "Error: Failed to create the user and group!"
     fi
 }
 
 function addRegularUser ()
 {
-    if [[ ! `grep "^${1}:x:" /etc/passwd` ]] then
+    declare -r USER=$1
+
+    if [[ ! isUser ]]
+    then
         message "Adding $1 to /etc/passwd"
-        useradd -d /home/$1 -e 2020-01-01 -c "Normal user." -s /bin/sh -U $1
+        useradd -d /home/${USER} -e 2021-01-01 -c "Normal user." -s /bin/sh -U $USER
         showUseraddReport $?
-        showUser $1
+        showUser $USER
         sleep 2
         return 0
     fi
@@ -266,7 +187,7 @@ function addAccounts ()
 
     for user in "$@"
     do
-        addRegularUser `echo $user | tr -d [:space:]`
+        addRegularUser $(trim $user)
     done
 }
 
@@ -281,16 +202,19 @@ function addAccountsFromFile ()
 
 function addGroup ()
 {
-    groupadd -g $2 $1
+    declare -r GID=$1
+    declare -r GROUP_NAME=$2
+
+    groupadd -g $GID $GROUP_NAME 
 }
 
 function addToGroup ()
 {
-    group=$1
+    declare -r GROUP=$1
     shift
 
     for $user in "$@"
     do
-        usermod -G $group $user
+        usermod -G $GROUP $user
     done
 }
